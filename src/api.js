685ce475bc378b9582889c08200a0ffb9e75a4fc
Woneeee <a6885088@naver.com>
2024-07-12 17:40:54 +0900

    api setting

---
+++ src/api.js
@@ -0,0 +1,18 @@
+const fetch = require("node-fetch");
+
+const baseUrl = "https://api.themoviedb.org/3/";
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZGVmNTZkYWIzNmRiZmQ4NWRiNzIzMTkzYzZjNzIzNyIsIm5iZiI6MTcyMDY4NjM4NC4zNDA4NDMsInN1YiI6IjY0ODY5ZjgxYzAzNDhiMDEzYzFkNTY3NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8nZDXY863GKt7_CATDZ3yE7-iRn13pQ9Xpnzb3q3rB8",
+  },
+};
+
+const url = (urlName) => {
+  return baseUrl + `${urlName}?language=ko-kr`;
+};
+
+export const nowPlaying = () =>
+  fetch(url("movie/now_playing"), options).then((res) => res.json());
